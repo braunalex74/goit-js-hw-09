22f0e054be4826f01a7cdeae969318f718266e5e
braunalex74 <braunalex74@googlemail.com>
2023-04-23 11:39:46 +0300

    DZ_erklarung

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -36,3 +36,49 @@ form.addEventListener('submit', event => {
       });
   }
 });
+
+// const commandInputs = {
+//   form: document.querySelector('.form'),
+// };
+
+// commandInputs.form.addEventListener('submit', getFormData);
+
+// function getFormData(evt) {
+//   evt.preventDefault();
+//   const { delay, step, amount } = evt.currentTarget.elements;
+//   const promisesArray = new Array(Number(amount.value)).fill().map(index => {
+//     const position = index + 1;
+//     const promiseDelay = Number(delay.value) + index * Number(step.value);
+
+//     return createPromise(position, promiseDelay);
+//   });
+
+//   console.log(promisesArray); // ok
+
+//   promisesArray.forEach(promise => {
+//     promise
+//       .then(({ position, delay }) => {
+//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//       });
+//   });
+// }
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const shouldResolve = Math.random() > 0.3;
+
+//       if (!shouldResolve) {
+//         reject({ position, delay });
+//       }
+//       const result = { position, delay };
+//       resolve(result);
+//       const output = document.createElement('p');
+//       output.textContent = JSON.stringify(result);
+//       document.body.appendChild(output);
+//     }, delay);
+//   });
+// }
